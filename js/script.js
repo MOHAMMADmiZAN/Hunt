5a10231cdab5772caace4a1fca575617da77b8de
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-05-16 06:52:06 +0600

    touch
    Signed-off-by: MOHAMMAD MIZAN <mohammad25524143@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-// $('#banner-part').slick({
-//     dots: false,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows:false,
-//
-// });
+$('#banner-part').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.arrow-prev'),
+    nextArrow: $('.arrow-next'),
+
+
+});
