969cf7d17770fe6a7bf286eddbbf621e5a7a20f3
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-05-16 06:15:09 +0600

    touch
    Signed-off-by: MOHAMMAD MIZAN <mohammad25524143@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-function person(name, age, height) {
-    this.name = name
-    this.age = age
-    this.height = height
-
-
-}
-
-let p1 = new person('Mohammad Mizanur Rahman', 27, 5.5 + "ft")
-console.log(p1)
-console.log(p1.name)
-console.log(p1.age)
-console.log(p1.height)
-
+// $('#banner-part').slick({
+//     dots: false,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows:false,
+//
+// });
