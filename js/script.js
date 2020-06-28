2da2378e85b669d5fce29b5e92939d0acaa091b1
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-06-28 09:43:26 +0600

    add Responsive
    Signed-off-by: MOHAMMAD MIZAN <mohammad25524143@gmail.com>

--- js/script.js
+++ js/script.js
@@ -7,6 +7,38 @@ $('#banner-part').slick({
     arrows: true,
     prevArrow: '<i class="fas fa-arrow-left prev"></i>',
     nextArrow: '<i class="fas fa-arrow-right next"></i>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 
 });
 // venobox//
@@ -123,10 +155,10 @@ $('.nav-item a').on('click', function () {
     }
 });
 
-//back to top fadetoogle//
-win.scroll(() => win.scrollTop() > 100 ? b2b.fadeIn() : b2b.fadeOut());
-//back to top effect//
-b2b.click(() => html_body.animate({scrollTop: 0}, 1500));
+// //back to top fadetoogle//
+// win.scroll(() => win.scrollTop() > 100 ? b2b.fadeIn() : b2b.fadeOut());
+// //back to top effect//
+// b2b.click(() => html_body.animate({scrollTop: 0}, 1500));
 
 
 // Smooth Scrolling //
