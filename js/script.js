f714d9f8e1c4cf410c784697c188f1e118a4d0e9
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-06-28 11:45:43 +0600

    add Responsive
    Signed-off-by: MOHAMMAD MIZAN <mohammad25524143@gmail.com>

--- js/script.js
+++ js/script.js
@@ -209,11 +209,43 @@ $('.th-slide').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
     arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     centerMode: true,
     centerPadding: '0',
     focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                // arrows: true,
+
+
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+
 });
 
 
